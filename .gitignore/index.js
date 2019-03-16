e3ceec285c72f5841c64209dd655c51fe1578ea7
jujujujujujujujujujuju <43450571+jujujujujujujujujujuju@users.noreply.github.com>
2019-03-16 12:55:22 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("San Andreas RP !help");
+    console.log("le bot est co");
+});
+
+bot.login("NTU2NDIxNzUzNDMwMjEyNjA4.D25iqw.rehzckolOPuC70tnPtmLhU5EoL4");
